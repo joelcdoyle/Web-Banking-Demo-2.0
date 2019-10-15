0c13cf34521af08571bb1fabba090c8365a21eb5
joelcdoyle <joelchristiandoyle@gmail.com>
2019-10-15 09:14:41 -0500

    3 files modified

--- common/ssn-input/ssn-input.directive.js
+++ common/ssn-input/ssn-input.directive.js
@@ -1,30 +1,49 @@
 export default function ssnInput() {
-  'ngInject';
+  "ngInject";
   return {
     controller: SsnInputCtrl,
-    templateUrl: './ssn-input.html',
+    templateUrl: "./ssn-input.html",
     controllerAs: "$ctrl",
-    require: ['^form'],
+    require: ["^form"],
     scope: {
       name: "@",
       model: "=",
-      label: "="
+      label: "=",
+      onEnter: "&"
     },
     bindToController: true,
-    link: function(scope, elem, attrs, ) {
-      let ngModel = elem.find('input').controller('ngModel');
+    link: function(scope, elem, attrs, form) {
+      scope.$ctrl.form = form[0];
 
-      ngModel.$parsers.push((value) => {
-        let numbers = value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,2})(\d{0,4})/);
-        ngModel.$setViewValue(!numbers[2] ? numbers[1] : numbers[1] + '-' + numbers[2] + (numbers[3] ? '-' + numbers[3] : ''));
+      let ngModel = elem.find("input").controller("ngModel");
+
+      ngModel.$parsers.push(value => {
+        let numbers = value
+          .replace(/\D/g, "")
+          .match(/(\d{0,3})(\d{0,2})(\d{0,4})/);
+        ngModel.$setViewValue(
+          !numbers[2]
+            ? numbers[1]
+            : numbers[1] +
+                "-" +
+                numbers[2] +
+                (numbers[3] ? "-" + numbers[3] : "")
+        );
         ngModel.$render();
-      })
+      });
     }
-  }
+  };
 }
 
 class SsnInputCtrl {
   constructor() {
-    
+    "ngInject";
+    var $ctrl = this;
+  }
+  showSuccess() {
+    return this.form[this.name].$valid;
+  }
+  enterPressed() {
+    this.onEnter();
   }
 }
