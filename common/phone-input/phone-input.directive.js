6340d690929122aa33f8f5dac643698674868139
joelcdoyle <joelchristiandoyle@gmail.com>
2019-10-15 09:03:33 -0500

    2 files modified

--- common/phone-input/phone-input.directive.js
+++ common/phone-input/phone-input.directive.js
@@ -1,32 +1,48 @@
 export default function phoneInput() {
-  'ngInject';
+  "ngInject";
   return {
     controller: PhoneInputCtrl,
-    templateUrl: './phone-input.html',
+    templateUrl: "./phone-input.html",
     controllerAs: "$ctrl",
-    require: ['^form'],
+    require: ["^form"],
     scope: {
       name: "@",
       model: "=",
-      label: "="
+      label: "=",
+      onEnter: "&"
     },
     bindToController: true,
-    link: function(scope, elem, attrs, ) {
-      let ngModel = elem.find('input').controller('ngModel');
+    link: function(scope, elem, attrs) {
+      scope.$ctrl.form = form[0];
+      let ngModel = elem.find("input").controller("ngModel");
 
-      ngModel.$parsers.push((value) => {
-        let numbers = value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
-        let phone = !numbers[2] ? numbers[1] : '(' + numbers[1] + ') ' + numbers[2] + (numbers[3] ? '-' + numbers[3] : '');
+      ngModel.$parsers.push(value => {
+        let numbers = value
+          .replace(/\D/g, "")
+          .match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+        let phone = !numbers[2]
+          ? numbers[1]
+          : "(" +
+            numbers[1] +
+            ") " +
+            numbers[2] +
+            (numbers[3] ? "-" + numbers[3] : "");
         ngModel.$setViewValue(phone);
         ngModel.$render();
         return phone;
-      })
+      });
     }
-  }
+  };
 }
 
 class PhoneInputCtrl {
   constructor() {
-    'ngInject';
+    "ngInject";
+  }
+  showSuccess() {
+    return this.form[this.name].$valid;
+  }
+  enterPressed() {
+    this.onEnter();
   }
 }
